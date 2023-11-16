12368311de24aff4b18d4f6e184f1d576b091223
Zak-hc <zhaci001@gmail.com>
2023-11-16 22:18:15 +0000

    task

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,4 +1,5 @@
 # Fix problem of high amount of requests
+
 exec {'replace' :
 	provider => shell,
 	command => 'sudo sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/"  /etc/default/nginx && sudo service nginx restart ',
